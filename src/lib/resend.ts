8e4178327d887af6b838533cbd125d2f42a52d11
cezarcozta <cezarcozta@gmail.com>
2025-05-27 10:42:35 -0300

    env key as string

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -2,7 +2,7 @@
 
 import { EmailTemplate } from '../components/EmailTemplate';
 import { Resend } from 'resend';
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY as string);
 
 export async function sendEmail(formData: FormData) {
     try {
