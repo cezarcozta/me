0a1251ffc3484f5537175741f6adccc1b1ce3bd8
cezarcozta <cezarcozta@gmail.com>
2025-05-27 09:44:50 -0300

    footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,9 +3,9 @@ import { Button } from "../ui/button";
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="flex items-center justify-around py-8 px-3">
-        <p className="text-foreground">César Augusto Costa - © Copyright 2025</p>
+    <footer className="w-full">
+      <div className="flex flex-col sm:flex-row items-center justify-center sm:justify-around gap-4 py-8 px-3">
+        <p className="text-foreground text-center">César Augusto Costa - © Copyright 2025</p>
 
         <Button variant="link" className="text-foreground" asChild>
           <Link href="/" >Feedback</Link>
